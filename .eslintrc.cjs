0b987917aca8a3717462c23f1bb6e5576f95295b
uestchxy <uestchxy@163.com>
2024-05-13 11:35:28 +0800

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    // 参考：https://www.npmjs.com/package/@ecomfe/eslint-config
+    extends: [
+        '@ecomfe/eslint-config/strict',
+    ],
+    rules: {
+        'no-console': 'warn',
+    },
+};
