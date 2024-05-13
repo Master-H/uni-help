8ca3ac1a8a1902fcf85841729198824379c520d1
uestchxy <uestchxy@163.com>
2024-05-13 11:46:01 +0800

    Create a.js

---
+++ test/a.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author master
+ *
+ */
+const checkBaseRule = require('./common/index');
+const xcxRule = require('./xcx/index');
+
+module.exports = (type, config) => {
+    checkBaseRule(config);
+    const ruleType = {
+        'xcx': xcxRule,
+    };
+    ruleType[type] && ruleType[type](config);
+};
